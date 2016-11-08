afd6f5260012bde16390a3f89c4627d461f4b53b
Michael Bornholdt Nielsen <jarryDk@users.noreply.github.com>
2016-11-08 18:16:08 +0100

    Create start.bat

---
+++ minecraft-server/start.bat
@@ -0,0 +1,10 @@
+@echo "* "
+@echo "* "
+@echo "* Nu starter vi MineCraft serveren Kys min mås"
+@echo "* "
+@echo "* "
+
+java -Xmx1024M -Xms1024M -jar minecraft_server.jar nogui
+#java -Xmx1024M -Xms1024M -jar minecraft_server.jar
+
+@pause
