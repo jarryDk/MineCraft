fbea110e87573e969e594583473b4cda3cf47ef3
Michael Bornholdt Nielsen <michaelbornholdtnielsen@gmail.com>
2021-02-21 17:08:22 +0100

    Layout is hard

--- android-minecraft/app/src/main/java/dk/jarry/minecraft/MainActivity.java
+++ android-minecraft/app/src/main/java/dk/jarry/minecraft/MainActivity.java
@@ -1,5 +1,7 @@
 package dk.jarry.minecraft;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -7,29 +9,36 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        /**
+        NavController navController = Navigation.findNavController(this, R.id.action_FirstFragment_to_SecondFragment);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
+*/
     }
 
     @Override
@@ -41,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -48,6 +58,10 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            return true;
+        }
+
+        if (id == R.id.action_navigate_page2) {
             return true;
         }
 
--- android-minecraft/app/src/main/java/dk/jarry/minecraft/SecondFragment.java
+++ android-minecraft/app/src/main/java/dk/jarry/minecraft/SecondFragment.java
@@ -1,9 +1,14 @@
 package dk.jarry.minecraft;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,24 +16,64 @@ import androidx.navigation.fragment.NavHostFragment;
 
 public class SecondFragment extends Fragment {
 
+    private SharedPreferences sharedpreferences;
+    private TextView name;
+    private TextView password;
+    public static final String mypreference = "mypref";
+    public static final String NAME = "nameKey";
+    public static final String PASSWORD = "passwordKey";
+
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
+            Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_second, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+/**
+        this.name = (TextView) view.findViewById(R.id.etName);
+        this.password = (TextView) view.findViewById(R.id.etPassword);
+        this.sharedpreferences = getContext().getSharedPreferences(mypreference, Context.MODE_PRIVATE);
 
-        view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
-            }
-        });
+        if (sharedpreferences.contains(NAME)) {
+            this.name.setText(sharedpreferences.getString(NAME, ""));
+        }
+        if (sharedpreferences.contains(PASSWORD)) {
+            this.password.setText(sharedpreferences.getString(PASSWORD, ""));
+        }
+ */
+    }
+
+    public void save(View view) {
+        String n = name.getText().toString();
+        String p = password.getText().toString();
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        editor.putString(NAME, n);
+        editor.putString(PASSWORD, p);
+        editor.commit();
+    }
+
+    public void clear(View view) {
+        name = (TextView) view.findViewById(R.id.etName);
+        password = (TextView) view.findViewById(R.id.etPassword);
+        name.setText("");
+        password.setText("");
+    }
+
+    public void get(View view) {
+        name = (TextView) view.findViewById(R.id.etName);
+        password = (TextView) view.findViewById(R.id.etPassword);
+        sharedpreferences = getContext().getSharedPreferences(mypreference, Context.MODE_PRIVATE);
+
+        if (sharedpreferences.contains(NAME)) {
+            name.setText(sharedpreferences.getString(NAME, ""));
+        }
+        if (sharedpreferences.contains(PASSWORD)) {
+            password.setText(sharedpreferences.getString(PASSWORD, ""));
+        }
     }
 }
